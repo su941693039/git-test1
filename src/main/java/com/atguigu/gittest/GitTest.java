6374abf93919557e2302b6513b25f0b5ef394723
Mrsu <941693039@qq.com>
2023-03-23 21:22:59 +0800

    hot-fix2 commit

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("222222");
-        System.out.println("hot-fix commit");
+        System.out.println("hot-fix1 commit1");
+        System.out.println("hot-fix2 commit2");
+
     }
 }
